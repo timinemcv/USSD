a258022ccd2e6cfd36e7f2a5481bb1efafe10772
timinemcv <110819699+timinemcv@users.noreply.github.com>
2022-08-25 23:05:12 +0100

    ussd

---
+++ airtime/main.js
@@ -0,0 +1,15 @@
+//airtime js
+
+let opr=parseInt(prompt("Welcome to *894# banking\n 1>Quick Banking\n 2>Open an account\n 3>Loans\n 4>Firstmonie\n 5>Payattitude"))
+
+if(opr===1){ 
+    let ans = parseInt(prompt("Welcome to *894# banking\n 1>Transfer money\n 2>Airtime(self)\n 3>Airtime(others)\n 4>Enquiry services\n 5>Data\n 6>Pay bills\n 7>Next page"))
+    let num1=parseInt(prompt("Enter amount"))
+    let message=parseInt(prompt("Enter mobile number"))
+    let message1=parseInt(prompt("select recipient number\n 1>MTN \n 2>GLO\n 3>Airtel \n4>Etisalat \n5>others"))
+    let ans1= parseInt(prompt("Recharge of N100 to 08062479300\n Please enter your 5 digit PIN:"))
+    alert("Successful. You can now increase your transfer limit from the comfort of your home. dial *894#, navigate to other services and follow the prompt. You Fi...")
+
+}
+
+    
---
+++ ussd transfer/main.js
@@ -0,0 +1,14 @@
+let opr= prompt("Welcome to *894# banking\n 1>Transfer money\n 2>Airtime(self)\n 3>Airtime(others)\n 4>Enquiry services\n 5>Data\n 6>Pay bills\n 7>Next page")
+let num1=parseInt(prompt("Enter Amount:"))
+if(opr===1){
+    let ans=num1
+   let message=parseInt(prompt("Select recipient bank\n 1>Zenith bank\n 2>Wema Bank\n 3>Standard Chartered\n 4>Providus Bank\n 5>Enter 3 letters of bank name")) 
+
+}
+let num2=parseInt(prompt("Enter Recipient Account:"))
+if(opr===2){
+    
+}
+{let ans1=parseInt(prompt("Transfer N500 to MICHEAL TIMILEYIN\n 2117185317 at N10.75 charge\nPlease enter your 5 digit PIN:"))
+}
+{alert("Successful. Stay Connected Always! Simply dial\n *894*Amount# to buy airtime for self of dial\n *894*amount*mobileNumber# to recharge for third\n party. You Fi...")}
